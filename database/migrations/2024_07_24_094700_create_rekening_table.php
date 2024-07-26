f525b9ef0dfbb1f99ace13d6b5bbf76f602dfc6c
Ilham-Nur <ilonoer26@gmail.com>
2024-07-26 17:34:43 +0700

    push frst

---
+++ database/migrations/2024_07_24_094700_create_rekening_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tbl_rekening', function (Blueprint $table) {
+            $table->id();
+            $table->string('pemilik');
+            $table->string('nomer_rekening');
+            $table->string('nama_bank');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('rekening');
+    }
+};
---
+++ resources/views/layout/app.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  {{-- <link href="img/logo/logo.png" rel="icon"> --}}
+  <title>@yield('title')</title>
+  <link href="{{ asset('RuangAdmin/vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
+  <link href="{{ asset('RuangAdmin/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css">
+  <link href="{{ asset('RuangAdmin/css/ruang-admin.min.css') }}" rel="stylesheet">
+</head>
+
+<body id="page-top">
+  <div id="wrapper">
+
+    @yield('content')
+
+  </div>
+
+  <!-- Scroll to top -->
+  <a class="scroll-to-top rounded" href="#page-top">
+    <i class="fas fa-angle-up"></i>
+  </a>
+  <script src="{{ asset('RuangAdmin/vendor/jquery/jquery.min.js') }}"></script>
+  <script src="{{ asset('RuangAdmin/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+  <script src="{{ asset('RuangAdmin/vendor/jquery-easing/jquery.easing.min.js') }}"></script>
+  <script src="{{ asset('RuangAdmin/js/ruang-admin.min.js') }}"></script>
+  <script src="{{ asset('RuangAdmin/vendor/chart.js/Chart.min.js') }}"></script>
+  <script src="{{ asset('RuangAdmin/js/demo/chart-area-demo.js') }}"></script>
+</body>
+
+</html>
