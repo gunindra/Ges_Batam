196736a74ba284fb0d5b5324183892b28e6668da
Ilham-Nur <ilonoer26@gmail.com>
2024-12-03 13:24:34 +0700

    perbaiki halaman main

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -284,27 +284,6 @@
                     <i class="fa fa-bars"></i>
                 </button>
                 <ul class="navbar-nav ml-auto">
-                    {{-- <li class="nav-item dropdown no-arrow">
-                        <a class="nav-link dropdown-toggle" href="#" id="searchDropdown" role="button"
-                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            <i class="fas fa-search fa-fw"></i>
-                        </a>
-                        <div class="dropdown-menu dropdown-menu-right p-3 shadow animated--grow-in"
-                            aria-labelledby="searchDropdown">
-                            <form class="navbar-search">
-                                <div class="input-group">
-                                    <input type="text" class="form-control bg-light border-1 small"
-                                        placeholder="What do you want to look for?" aria-label="Search"
-                                        aria-describedby="basic-addon2" style="border-color: #3f51b5;">
-                                    <div class="input-group-append">
-                                        <button class="btn btn-primary" type="button">
-                                            <i class="fas fa-search fa-sm"></i>
-                                        </button>
-                                    </div>
-                                </div>
-                            </form>
-                        </div>
-                    </li> --}}
                     <li class="nav-item dropdown no-arrow mx-1">
                         <a class="nav-link dropdown-toggle" href="#" id="alertsDropdown" role="button"
                             data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
@@ -362,7 +341,7 @@
                             <a class="dropdown-item text-center small text-gray-500" href="">View all Kuota</a>
                         </div>
                     </li>
-                    {{-- <li class="nav-item dropdown no-arrow mx-1">
+                    <!-- <li class="nav-item dropdown no-arrow mx-1">
                         <a class="nav-link dropdown-toggle" href="#" id="messagesDropdown" role="button"
                             data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                             <i class="fas fa-envelope fa-fw"></i>
