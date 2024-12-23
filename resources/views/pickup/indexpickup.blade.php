e1c9cceba99b5453a609d4261a6ea6a82fa9f0ce
Ilham-Nur <ilonoer26@gmail.com>
2024-12-23 13:57:45 +0700

    push to test

--- resources/views/pickup/indexpickup.blade.php
+++ resources/views/pickup/indexpickup.blade.php
@@ -46,7 +46,7 @@
         }
     </style>
 
-
+ {{-- Pickup --}}
     <!-- Modal -->
     <div class="modal fade" id="passwordModal" tabindex="-1" aria-labelledby="passwordModalLabel" aria-hidden="true">
         <div class="modal-dialog modal-dialog-centered">
