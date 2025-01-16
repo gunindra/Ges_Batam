47eec2457ed98eaf97b16df4dd92e65635f1303f
Ilham-Nur <ilonoer26@gmail.com>
2025-01-16 16:21:35 +0700

    push middleware

--- app/Http/Middleware/SetActiveCompany.php
+++ app/Http/Middleware/SetActiveCompany.php
@@ -4,14 +4,25 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+
 class SetActiveCompany
 {
     public function handle(Request $request, Closure $next)
     {
-        // Periksa apakah session `active_company_id` ada
-        if (!session()->has('active_company_id')) {
-            // Jika tidak ada, tetapkan nilai default 1
+        $activeCompany = \DB::table('tbl_company')
+        ->where('is_active', 1)
+        ->first();
+
+        if (!$activeCompany) {
+            // Jika tidak ada yang aktif, tetapkan default 1
             session(['active_company_id' => 1]);
+
+            // Perbarui `is_active` di `tbl_company` untuk id 1
+            \DB::table('tbl_company')
+                ->where('id', 1)
+                ->update(['is_active' => 1]);
+        } else {
+            session(['active_company_id' => $activeCompany->id]);
         }
 
         // Perbarui session jika ada `company_id` di query string
