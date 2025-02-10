4882d5339cc6d9b833dff58aea50a60e9092dcc7
Ilham-Nur <ilonoer26@gmail.com>
2025-02-10 15:37:54 +0700

    push search cotumer di filter di soa costmer

--- resources/views/Report/Soa/indexbalance.blade.php
+++ resources/views/Report/Soa/indexbalance.blade.php
@@ -4,6 +4,28 @@
 
 @section('main')
 
+    <style>
+        .select2-container--default .select2-selection--single {
+            height: 40px;
+            border: 1px solid #d1d3e2;
+            border-radius: 0.25rem;
+            padding: 6px 12px;
+        }
+
+        .select2-container--default .select2-selection--single .select2-selection__rendered {
+            line-height: 27px;
+        }
+
+        .select2-container--default .select2-selection--single .select2-selection__arrow {
+            height: 38px;
+        }
+
+        .select2-dropdown {
+            border: 1px solid #ced4da;
+            border-radius: 0.25rem;
+        }
+    </style>
+
     <div class="container-fluid" id="container-wrapper">
         <div class="d-sm-flex align-items-center justify-content-between mb-4">
             <h1 class="h3 mb-0 text-gray-800">Statement of Account</h1>
@@ -31,16 +53,18 @@
                         <div class="row">
                             <div class="col-12">
                                 <div class="mt-3">
-                                    <label for="Tanggal" class="form-label fw-bold">Customer:</label>
+                                    <label for="customer" class="form-label fw-bold">Customer:</label>
                                     <select class="form-control select2" id="customer">
-                                        <option value="" selected disabled>Pilih Customer</option>
+                                        <option value="" selected disabled>Pilih Costumer</option>
                                         @foreach ($customers as $customer)
-                                            <option value="{{ $customer->id }}">{{ $customer->marking }} - {{$customer->nama_pembeli}}</option>
+                                            <option value="{{ $customer->id }}">{{ $customer->marking }} -
+                                                {{ $customer->nama_pembeli }}</option>
                                         @endforeach
                                     </select>
                                 </div>
                                 <div class="mt-3">
-                                    <label for="Tanggal" class="form-label fw-bold">Pilih Tanggal: ( Kosongkan jika ingin munculkan semua )</label>
+                                    <label for="Tanggal" class="form-label fw-bold">Pilih Tanggal: ( Kosongkan jika ingin
+                                        munculkan semua )</label>
                                     <div class="d-flex align-items-center">
                                         <input type="date" id="startDate" class="form-control"
                                             placeholder="Pilih tanggal mulai" style="width: 200px;">
@@ -67,7 +91,8 @@
                     <div class="card-body">
                         <div class="d-flex mb-2 mr-3 float-right">
                             <button class="btn btn-primary mr-2" id="exportBtn">Export Excel</button>
-                            <a class="btn btn-success mr-1" style="color:white;" id="sendWA"><span class="pr-2"><i class="fab fa-whatsapp"></i></span>Send Whatsapp</a>
+                            <a class="btn btn-success mr-1" style="color:white;" id="sendWA"><span class="pr-2"><i
+                                        class="fab fa-whatsapp"></i></span>Send Whatsapp</a>
                         </div>
                         <div class="d-flex mb-2 mr-3 mb-4">
                             <button class="btn btn-primary ml-2" id="filterTanggal">Filter</button>
@@ -99,7 +124,7 @@
                 const startDate = $('#startDate').val();
                 const endDate = $('#endDate').val();
                 const customer = $('#customer').val();
-                
+
                 $.ajax({
                         url: "{{ route('getSoa') }}",
                         method: "GET",
@@ -140,14 +165,25 @@
                     var endDate = new Date(dateStr);
                     if (endDate < startDate) {
                         showwMassage(error,
-                        "Tanggal akhir tidak boleh lebih kecil dari tanggal mulai.");
+                            "Tanggal akhir tidak boleh lebih kecil dari tanggal mulai.");
                         $('#endDate').val('');
                     }
                 }
             });
 
+
             $(document).on('click', '#filterTanggal', function(e) {
                 $('#modalFilterTanggal').modal('show');
+            });
+
+            $('.select2').select2();
+
+            $('#customer').select2({
+                placeholder: "Pilih Customer",
+                allowClear: true,
+                width: '100%',
+                minimumResultsForSearch: 0,
+                dropdownParent: $('#modalFilterTanggal')
             });
 
             $('#saveFilterTanggal').click(function() {
@@ -161,52 +197,55 @@
                 const customer = $('#customer').val();
 
                 // Construct URL with query parameters
-                const pdfUrl = `{{ route('soaWA') }}?startDate=${startDate}&endDate=${endDate}&customer=${customer}`;
+                const pdfUrl =
+                    `{{ route('soaWA') }}?startDate=${startDate}&endDate=${endDate}&customer=${customer}`;
                 window.location.href = pdfUrl;
             });
             $('#exportBtn').on('click', function() {
-            var startDate = $('#startDate').val();
-            var endDate = $('#endDate').val();
-            var customer = $('#customer').val();
+                var startDate = $('#startDate').val();
+                var endDate = $('#endDate').val();
+                var customer = $('#customer').val();
 
-            var now = new Date();
-            var day = String(now.getDate()).padStart(2, '0');
-            var month = now.toLocaleString('default', { month: 'long' });
-            var year = now.getFullYear();
-            var hours = String(now.getHours()).padStart(2, '0');
-            var minutes = String(now.getMinutes()).padStart(2, '0');
-            var seconds = String(now.getSeconds()).padStart(2, '0');
+                var now = new Date();
+                var day = String(now.getDate()).padStart(2, '0');
+                var month = now.toLocaleString('default', {
+                    month: 'long'
+                });
+                var year = now.getFullYear();
+                var hours = String(now.getHours()).padStart(2, '0');
+                var minutes = String(now.getMinutes()).padStart(2, '0');
+                var seconds = String(now.getSeconds()).padStart(2, '0');
 
-            var filename = `Soa Customer_${day} ${month} ${year} ${hours}:${minutes}:${seconds}.xlsx`;
+                var filename = `Soa Customer_${day} ${month} ${year} ${hours}:${minutes}:${seconds}.xlsx`;
 
-            $.ajax({
-                url: "{{ route('exportSoaCustomer') }}",
-                type: 'GET',
-                data: {
-                    startDate: startDate,
-                    endDate: endDate,
-                    nama_pembeli: customer
-                },
-                xhrFields: {
-                    responseType: 'blob'
-                },
-                success: function(data) {
-                    var blob = new Blob([data], {
-                        type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
-                    });
-                    var link = document.createElement('a');
-                    link.href = window.URL.createObjectURL(blob);
-                    link.download = filename;
-                    link.click();
-                },
-                error: function() {
-                    Swal.fire({
-                        title: "Export failed!",
-                        icon: "error"
-                    });
-                }
+                $.ajax({
+                    url: "{{ route('exportSoaCustomer') }}",
+                    type: 'GET',
+                    data: {
+                        startDate: startDate,
+                        endDate: endDate,
+                        nama_pembeli: customer
+                    },
+                    xhrFields: {
+                        responseType: 'blob'
+                    },
+                    success: function(data) {
+                        var blob = new Blob([data], {
+                            type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+                        });
+                        var link = document.createElement('a');
+                        link.href = window.URL.createObjectURL(blob);
+                        link.download = filename;
+                        link.click();
+                    },
+                    error: function() {
+                        Swal.fire({
+                            title: "Export failed!",
+                            icon: "error"
+                        });
+                    }
+                });
             });
-        });
 
         });
     </script>
