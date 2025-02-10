a3bbb71a577ca341217af36f1f055cac8903e8f7
Ilham-Nur <ilonoer26@gmail.com>
2025-02-10 10:42:53 +0700

    push totest

--- resources/views/customer/invoice/buatinvoice.blade.php
+++ resources/views/customer/invoice/buatinvoice.blade.php
@@ -265,7 +265,7 @@
                                 <span id="total-harga" style="font-weight: bold; color: #555;">Rp. 0</span>
                             </div>
                             <small class="text-muted d-block mt-1">
-                                NB: Setelah diproses total harga akan dibulatkan ke kelipatan 1000.
+                                NB: Setelah diproses, total harga akan dibulatkan ke atas dalam kelipatan 1000.
                             </small>
                             <input type="hidden" name="" id="totalHargaValue">
                             <button id="buatInvoice" class="btn btn-primary p-3 float-right mt-3" style="width: 100%;">Buat Invoice</button>
