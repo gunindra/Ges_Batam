c711564e8255a144e853c5ee8565d220166fe7c5
Ilham-Nur <ilonoer26@gmail.com>
2025-04-21 15:19:10 +0700

    push chek user active dan update expired setiap hari di jam 00:00

--- routes/console.php
+++ routes/console.php
@@ -5,8 +5,8 @@ use Carbon\Carbon;
 use Illuminate\Console\Scheduling\Schedule;
 use App\Console\Commands\CheckUserActivity;
 
-app(Schedule::class)->command(CheckUserActivity::class)->everyMinute();
-app(Schedule::class)->command(UpdateExpiredTopups::class)->everyMinute();
+app(Schedule::class)->command(CheckUserActivity::class)->dailyAt('00:00');
+app(Schedule::class)->command(UpdateExpiredTopups::class)->dailyAt('00:00');
 app(Schedule::class)->command('journal:create-monthly')->monthlyOn(28, '23:59') // Set a placeholder date like the 28th to avoid months with fewer days
                                                         ->when(function () {
                                                             return Carbon::now()->endOfMonth()->isToday();
