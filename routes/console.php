f95bbbac7facfd6194fddff63c05837e065a3b96
Ilham-Nur <ilonoer26@gmail.com>
2025-04-21 15:12:31 +0700

    update schedular cke expired topup

--- routes/console.php
+++ routes/console.php
@@ -6,9 +6,9 @@ use Illuminate\Console\Scheduling\Schedule;
 use App\Console\Commands\CheckUserActivity;
 
 app(Schedule::class)->command(CheckUserActivity::class)->everyMinute();
+app(Schedule::class)->command(UpdateExpiredTopups::class)->everyMinute();
 app(Schedule::class)->command('journal:create-monthly')->monthlyOn(28, '23:59') // Set a placeholder date like the 28th to avoid months with fewer days
                                                         ->when(function () {
                                                             return Carbon::now()->endOfMonth()->isToday();
                                                         });
 app(Schedule::class)->command('queue:work --daemon')->everyMinute();
-app(Schedule::class)->command(UpdateExpiredTopups::class)->everyMinute();
