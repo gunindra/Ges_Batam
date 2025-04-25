f31782dcb4bc15b307101b2b21782bb28d9950f6
BopeLxLapchiong <studiobopel@gmail.com>
2025-04-25 16:25:26 +0700

    update

--- routes/console.php
+++ routes/console.php
@@ -10,6 +10,7 @@ app(Schedule::class)->command(UpdateExpiredTopups::class)->dailyAt('00:00');
 app(Schedule::class)->command('journal:create-monthly')
     ->dailyAt('16:23')
     ->when(function () {
-        return Carbon::now()->isSameDay(Carbon::now()->endOfMonth());
+        return Carbon::now()->day === 25;
+        // return Carbon::now()->isSameDay(Carbon::now()->endOfMonth());
     });
 app(Schedule::class)->command('queue:work --daemon')->everyMinute();
