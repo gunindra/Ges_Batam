9272cf06db8b16e59ae4d5d02936222e803f9568
BopeLxLapchiong <studiobopel@gmail.com>
2025-08-22 14:14:35 +0700

    update

--- routes/console.php
+++ routes/console.php
@@ -8,9 +8,9 @@ use App\Console\Commands\CheckUserActivity;
 app(Schedule::class)->command(CheckUserActivity::class)->dailyAt('00:00');
 app(Schedule::class)->command(UpdateExpiredTopups::class)->dailyAt('00:00');
 app(Schedule::class)->command('journal:create-monthly')
-    ->dailyAt('07:00')
-    ->when(function () {
-        // return Carbon::now()->day === 25;
-        return Carbon::now()->isSameDay(Carbon::now()->endOfMonth());
-    });
+    ->dailyAt('14:16');
+    // ->when(function () {
+    //     Carbon::today()->setTime(14, 0, 0);
+    //     // return Carbon::now()->isSameDay(Carbon::now()->endOfMonth());
+    // });
 app(Schedule::class)->command('queue:work --daemon')->everyMinute();
